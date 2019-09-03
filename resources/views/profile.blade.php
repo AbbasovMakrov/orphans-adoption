6cd661d4435cd36597e9e215f812948782a97833
abbasWEb <abbasaljoker@gmail.com>
2019-09-03 03:24:08 +0300

    final

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,9 +3,9 @@
    <div class ="col-md-12">
    <form action="post" action="{{ route ("profile.update")) }}">
     @csrf
-   <input class="form-control" type="text" placeholder="Name" value="{{auth()->user()->name}}">
-   <input class="form-control" type="email" placeholder="Email" value="{{auth()->user()->email}}">
-   <input class="form-control" type="password" placeholder="Password">
+   <input class="form-control" name="name" type="text" placeholder="Name" value="{{auth()->user()->name}}">
+   <input class="form-control" name="email" type="email" placeholder="Email" value="{{auth()->user()->email}}">
+   <input class="form-control" name="password" type="password" placeholder="Password">
     <locations></locations>
        <input type="number" name="age" value="{{auth()->user()->age}}" placeholder="Phone" class="form-control" >
        <input type="number" name="phone" class="form-control" value="{{auth()->user()->phone}}" placeholder="Phone">
